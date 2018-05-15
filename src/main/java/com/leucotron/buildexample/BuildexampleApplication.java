b2acbcd9dc03d6222ffd884ebeafe5436ee26842
Edgard 1010257 <edgard.lopes@leucotron.com.br>
2018-05-15 16:03:32 -0300

    first commit

---
+++ src/main/java/com/leucotron/buildexample/BuildexampleApplication.java
@@ -0,0 +1,22 @@
+package com.leucotron.buildexample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildexampleApplication implements CommandLineRunner {
+
+    @Value("${build.number}")
+    private String buildNumber;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildexampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("BUILD NUMBER GERADO: " + buildNumber);
+    }
+}
